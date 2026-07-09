ca2998f4945ca05795b9f1b097261bd8a1f50e25
Sean Bruen <kyau@kyau.net>
2026-07-09 15:53:26 -0700

    style(eval): remove duplicate RCS header and vim modeline from test file
    Same dupes cleaned from RunnerTest.php as fd13eab did for EvalRunner.php.
    
    Plan-by: glm-5.2
    Acked-by: deepseek-v4-pro
    Signed-off-by: kyau <git@kyaulabs.com>

--- tests/Unit/Eval/RunnerTest.php
+++ tests/Unit/Eval/RunnerTest.php
@@ -2,10 +2,7 @@
 
 # $KYAULabs: RunnerTest.php kyau@akira.kyaulabs 2026/07/09 -0700 Exp $
 
-
 declare(strict_types=1);
-
-# $KYAULabs: RunnerTest.php kyau@nova 2026/07/05 -0700 Exp $
 
 use KYAULabs\Eval\Runner;
 use KYAULabs\Eval\EvalCase;
@@ -716,7 +713,5 @@ it('deterministic gate: output contains expected string fails when expectedStrin
     expect($result->deterministicChecks['expected_string']['pass'])->toBeFalse();
     expect($result->deterministicChecks['expected_string']['found'])->toBeFalse();
 });
-
-// vim: ft=php sts=4 sw=4 ts=4 et :
 
 // vim: ft=php sts=4 sw=4 ts=4 et :
